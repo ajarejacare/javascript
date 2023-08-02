54fe6d62b58f5a5dcdd0052f13b0cc0f872375a8
Eric Rizzo Cesar <106925782+ajarejacare@users.noreply.github.com>
2023-08-02 12:41:34 -0300

    Add files via upload

---
+++ aula-js/forms.js
@@ -0,0 +1,14 @@
+function enviar(){
+     var nome = document. getElementById("nomeid");
+     var telefone= document.getAnimations("foneid")
+     var email= document.getAnimations("emailid")
+     
+     if(nome.value != ""){
+        alert('bom dia sr(a) '+ nome.value + ' sua clonagem de cartão foi bem feita') 
+     }
+     else{
+        alert('COLOCA A PORRA DO EU NOME')
+     }
+
+
+}
